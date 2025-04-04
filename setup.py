07a888108683ddce4c19a6d8c77afbdd4864d321
Peter Peterson <peter@arborxr.com>
2025-04-03 18:59:46 -0600

    update version to 0.9.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name = 'abxrcli',
-    version = '0.9.1',
+    version = '0.9.2',
     packages = ['abxr'],
     entry_points = {
         'console_scripts': [
