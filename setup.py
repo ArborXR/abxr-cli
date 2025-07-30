6050ca924630b15930ac1fdb9e14f2f8223c8af0
Peter Peterson <peter@arborxr.com>
2025-07-29 22:16:56 -0600

    added ability to upload files and assign them to devices or groups

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name = 'abxrcli',
-    version = '0.11.0',
+    version = '0.12.0',
     packages = ['abxr'],
     entry_points = {
         'console_scripts': [
