fece512ad6cf196e86b099823dd91877fe468f28
Brian Rice <brian@arborxr.com>
2026-02-10 10:28:02 -0500

    replace deprecated pkg_resources with importlib.metadata
    pkg_resources has been removed in Python 3.14 and emits deprecation
    warnings in Python 3.13. Use importlib.metadata (available since 3.8)
    to resolve the package version instead. Bump version to 0.15.3.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name = 'abxrcli',
-    version = '0.15.2',
+    version = '0.15.3',
     packages = ['abxr'],
     entry_points = {
         'console_scripts': [
