ea289de825f0893cec9d8feb649ed0e88ab5209e
Brian Rice <brian@arborxr.com>
2026-04-13 13:26:43 -0400

    feat: add --bundle_id flag to release_channel_set_version command
    Allows targeting app bundles (APK + OBB) directly when updating a
    release channel, mutually exclusive with --version_id. Bumps version
    to 0.17.0.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name = 'abxrcli',
-    version = '0.16.1',
+    version = '0.17.0',
     packages = ['abxr'],
     entry_points = {
         'console_scripts': [
